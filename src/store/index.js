b72447b7320f6303726e8dc1d193d961933c48de
DmitruVasilev <mr.disproziu91@gmail.com>
2018-12-17 19:07:41 +0500

    Add logger, dev tools, hmr

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,33 @@
-import {createStore, applyMiddleware} from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
+import logger from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import rootReducer from "../reducers";
 
-export default function configureStore() {
-  return createStore(
-    rootReducer,
+const middlewares = [thunkMiddleware]
+
+let composeEnhancers = compose
+
+if (process.env.NODE_ENV === `development`) {
+  middlewares.push(logger);
+
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({serialize: true})
+    : compose;
+}
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
     applyMiddleware(
-      thunkMiddleware
+      ...middlewares
     )
   )
+)
+
+export default function configureStore() {
+  if(module.hot){
+    module.hot.accept('../reducers', ()=> {
+      store.repleceReducer(rootReducer)
+    })
+  }
+  return store
 }
